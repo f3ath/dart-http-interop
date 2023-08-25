5c3addef649df0b0bd17ac74fd65d179afcbeeb4
f3ath <karapetov@gmail.com>
2023-08-24 20:37:34 -0700

    release

--- test/headers_test.dart
+++ test/headers_test.dart
@@ -4,7 +4,9 @@ import 'package:test/test.dart';
 void main() {
   group('Headers', () {
     test('are case-insensitive', () {
-      final headers = Headers({'Accept': ['text/plain']});
+      final headers = Headers({
+        'Accept': ['text/plain']
+      });
       expect(headers['Accept']?.first, equals('text/plain'));
       expect(headers['accept']?.first, equals('text/plain'));
       expect(headers['AcCePt']?.first, equals('text/plain'));
