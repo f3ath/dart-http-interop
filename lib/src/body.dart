1dd6c410faf57f5c64f964a9f6e8fe90853112a9
Alexey <f3ath@users.noreply.github.com>
2023-10-16 08:47:57 -0700

    Empty body should produce empty stream (#12)

--- lib/src/body.dart
+++ lib/src/body.dart
@@ -6,14 +6,15 @@ import 'dart:typed_data';
 /// useful constructors.
 class Body {
   /// Creates a new empty body.
-  Body() : this.binary(Uint8List(0));
+  Body() : this.stream(Stream.empty());
 
   /// Create a new instance from [text] encoded with [encoding].
   Body.text(String text, Encoding encoding)
       : this.binary(Uint8List.fromList(encoding.encode(text)));
 
   /// Create a new instance from a single piece of binary [data]..
-  Body.binary(Uint8List data) : this.stream(Stream.value(data));
+  Body.binary(Uint8List data)
+      : this.stream(data.isEmpty ? Stream.empty() : Stream.value(data));
 
   /// Creates a new instance from a binary stream.
   Body.stream(this.bytes);
